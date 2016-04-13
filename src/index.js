1a45fd42c02d0a7113fe503e8c404143fde00188
Ron Waldon <jokeyrhyme@gmail.com>
2016-04-13 21:47:57 +1000

    use Web Font Loader for asynchronous fonts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import WebFont from 'webfontloader';
 
 import { App } from './components/App.js';
 
 ReactDOM.render(<App />, document.querySelector('main'));
+
+WebFont.load({
+  classes: false,
+  events: false,
+  google: {
+    families: ['Source Sans Pro']
+  }
+});
